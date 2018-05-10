84baf869e507086811f79af491262d8f5e709234
SaraRiach <saracar2@gmail.com>
2018-05-10 16:47:54 +0200

    finally can add batches in state and db

---
+++ client/src/reducers/batch.js
@@ -0,0 +1,26 @@
+import {POST_BATCH, GET_BATCH, GET_BATCHES} from '../actions/batches'
+
+/*
+The state will contain the games in an object with the game ID as key
+*/
+
+export default (state = [], {type, payload}) => {
+  switch (type) {
+    case GET_BATCH:
+      return payload
+
+    case GET_BATCHES:
+      return payload
+      
+    case POST_BATCH:
+      return 
+        [...state, payload]
+      
+
+    
+    
+
+    default:
+      return state
+  }
+}
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,13 @@
-// import games from './games'
+import batches from './batch'
 // import users from './users'
 import login from './login'
-// import currentUser from './currentUser'
+import currentUser from './currentUser'
 import signup from './signup'
 
 export default {
   
   signup,
-  login
+  login,
+  batches,
+  currentUser
 }
